22e0597286fab5fd86b1b04aea462dc92afac15b
Christopher Collins <chris@spring215.com>
2014-10-25 13:42:24 -0400

    final fixes

--- contact_form.php
+++ contact_form.php
@@ -3,12 +3,12 @@
 $contact_name = $_POST['contact_name'];
 $contact_email = $_POST['contact_email'];
 $contact_message = $_POST['contact_message'];
-$to = 'chris@spring215.com';
+$to = 'info@jporlewicz.com';
 $subject = 'JP Website Message';
 
 $message = 'CONTACT NAME: ' . $contact_name . 'EMAIL: ' . $contact_email . "\n" . 'MESSAGE: ' . $contact_message;
 
 mail ($to, $subject, $message, 'From: ' . $contact_name);
-header("Location: http://www.website.com/thank-you.html");
+header("Location: http://www.jporlewicz.com/thank-you.html");
 
 ?>
